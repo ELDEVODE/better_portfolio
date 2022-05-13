9e5c2bb85e0bde3a4b75025897c0a21bc52a2d26
ELDEVODE01~ <elpraise20@gmail.com>
2022-05-13 10:53:57 +0100

    Finished Navbar component

---
+++ frontend_react/src/App.jsx
@@ -0,0 +1,19 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss'
+
+const App = () =>  {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Testimonial />
+      <Footer />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
