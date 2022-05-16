f0bba6f43827bc906e2fc0e0307435ab1f135d12
ELDEVODE01~ <elpraise20@gmail.com>
2022-05-16 22:53:58 +0100

    changed design

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,32 @@
-import './About.scss'
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import './About.scss';
+
+const abouts = [
+  {
+    title: 'Frontend Development', description: 'I am a good Frontend developer.', imgUrl: ''
+  },
+  {
+    title: 'Web Development', description: 'I am a good web developer.', imgUrl: ''
+  }
+]
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
-export default About
+    <>
+      <h2 className="head-text">
+        I know that 
+        <span> Good Design</span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+
+      </div>
+    </>
+  );
+};
+export default About;
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.javascriptx,images.sass,images.reacth].map((circle,index) => (
+        {[images.javascriptx,images.reacth,images.sass].map((circle,index) => (
           <div className="circle-cmp app__flex" key={`circle-index`}>
             <img src={circle} alt="circle" />
           </div>
