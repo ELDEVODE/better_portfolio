4fb8de2cf347998815b6116569e8cf56a5873723
ELDEVODE01~ <elpraise20@gmail.com>
2022-05-18 20:47:09 +0100

    Arranged the high order components

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+import AppWrap from '../../wrapper/AppWrap';
 
 import './About.scss';
 import { urlFor, Client } from '../../client';
@@ -50,4 +50,4 @@ const About = () => {
     </>
   );
 };
-export default About;
+export default AppWrap(About, 'about');
