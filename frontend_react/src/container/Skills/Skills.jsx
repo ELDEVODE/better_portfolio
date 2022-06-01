2c41da20a3baa74e69beaf33be22ebc0d5ff16f3
ELDEVODE01~ <elpraise20@gmail.com>
2022-06-01 13:34:33 +0100

    Works section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,7 +1,53 @@
-import './Skills.scss'
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+
+import AppWrap from '../../wrapper/AppWrap';
+import { urlFor, Client } from '../../client';
+
+import './Skills.scss';
 const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type =="experiences"]';
+    const skillsQuery = '*[_type =="skills"]';
+
+    Client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    Client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
   return (
-    <div>Skills</div>
-  )
-}
-export default Skills
+    <>
+      <h2 className="head-text">Skills and Experience</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skilss-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
+};
+export default Skills;
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -21,7 +21,20 @@ const Work = () => {
     });
   }, []);
 
-  const handleWorkFilter = (item) => {};
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{y:0,opacity:1}])
+
+      if(item === 'All') {
+        setFilterWork(works)
+      }else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    },500)
+  };
 
   return (
     <>
@@ -32,7 +45,7 @@ const Work = () => {
         Section
       </h2>
       <div className="app__work-filter">
-        {['All','Web App', 'React JS' ].map((item, index) => (
+        {['All', 'Web App', 'React JS'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
@@ -51,7 +64,7 @@ const Work = () => {
         className="app__work-portfolio"
       >
         {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
+          <div className="app__work-item  app__flex" key={index}>
             <div className="app__work-img app_flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
